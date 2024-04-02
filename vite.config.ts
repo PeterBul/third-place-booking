5d60e404a092488cc34522dbf8e465335ee48dfc
PeterBul <pc.bulukin@gmail.com>
2024-04-02 11:13:56 +0200

    Fix live reloading with docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+    hmr: {
+      clientPort: 8080,
+    },
     port: 8080,
     strictPort: true,
-    origin: "http://0.0.0.0:8080",
+    origin: 'http://0.0.0.0:8080',
     watch: {
       usePolling: true,
     },
@@ -18,4 +21,3 @@ export default defineConfig({
     strictPort: true,
   },
 });
-
